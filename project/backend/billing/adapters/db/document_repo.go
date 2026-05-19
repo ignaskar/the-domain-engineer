92399285404f799dca7bee7dc217d36eb0f520ec
Three Dots Labs <contact@threedotslabs.com>
2026-05-19 22:05:06 +0300

    completed 04-implementing-aggregates/04-project-create-document

--- project/backend/billing/adapters/db/document_repo.go
+++ project/backend/billing/adapters/db/document_repo.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"eats/backend/billing/adapters/db/dbmodels"
+	"eats/backend/common"
+
+	pgx "github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"eats/backend/billing/domain"
@@ -28,5 +32,37 @@ func (r *PostgresRepository) CreateDocument(
 	series domain.DocumentSeries,
 	createFunc func(documentNumber domain.DocumentNumber) (DocumentRecord, error),
 ) (domain.DocumentUUID, error) {
-	return domain.DocumentUUID{}, fmt.Errorf("TODO: implement")
+	var docUUID domain.DocumentUUID
+	err := common.UpdateInReadCommittedTx(ctx, r.db, func(ctx context.Context, tx pgx.Tx) error {
+		queries := dbmodels.New(tx)
+
+		n, err := queries.NextDocumentNumber(ctx, series.String())
+		if err != nil {
+			return fmt.Errorf("next document number failed: %w", err)
+		}
+
+		docNumber, err := domain.NewDocumentNumber(series, int(n))
+		if err != nil {
+			return err
+		}
+
+		docRecord, err := createFunc(docNumber)
+		if err != nil {
+			return err
+		}
+
+		err = queries.SaveDocument(ctx, dbmodels.SaveDocumentParams{
+			DocumentUuid:   docRecord.UUID,
+			DocumentNumber: docNumber.String(),
+			SeriesPrefix:   series.String(),
+		})
+
+		docUUID = docRecord.UUID
+		return nil
+	})
+	if err != nil {
+		return domain.DocumentUUID{}, err
+	}
+
+	return docUUID, nil
 }
--- project/backend/billing/adapters/db/queries/documents.sql
+++ project/backend/billing/adapters/db/queries/documents.sql
@@ -1,1 +1,17 @@
--- TODO: implement
+-- name: NextDocumentNumber :one
+UPDATE billing.document_series
+SET last_number = last_number + 1,
+    updated_at = NOW()
+WHERE prefix = $1
+RETURNING last_number;
+
+-- name: SaveDocument :exec
+INSERT INTO billing.documents (
+    document_uuid,
+    document_number,
+    series_prefix
+) VALUES (
+    $1,
+    $2,
+    $3
+);
