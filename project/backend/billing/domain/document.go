1d92f54fdd2fa42840a34b49967bee30009d8a93
Three Dots Labs <contact@threedotslabs.com>
2026-06-08 11:27:06 +0300

    init files for 05-domain-modeling/03-project-issue-receipt-domain

--- project/backend/billing/domain/document.go
+++ project/backend/billing/domain/document.go
@@ -1,7 +1,48 @@
 package domain
 
-import "eats/backend/common"
+import (
+	"errors"
+	"time"
+
+	"eats/backend/common"
+	"eats/backend/common/shared"
+)
+
+type DocumentType struct {
+	common.Enum[DocumentTypeValues]
+}
+
+type DocumentTypeValues string
+
+func (DocumentTypeValues) Values() []string {
+	return []string{"receipt"}
+}
+
+var DocumentTypeReceipt = common.MustEnum[DocumentType]("receipt")
+
+func NewReceipt(data NewDocumentData, docNumber DocumentNumber) (*Document, error) {
+	// TODO implement me
+	return nil, errors.New("not implemented")
+}
+
+type NewDocumentData struct {
+	ExternalReference *string
+	IssueDate         time.Time
+	Currency          shared.Currency
+	Seller            LegalEntity
+	Buyer             LegalEntity
+	LineItems         []NewLineItemData
+}
+
+type NewLineItemData struct {
+	Name     string
+	Quantity int
+}
 
 type DocumentUUID struct {
+	common.UUID
+}
+
+type LineItemUUID struct {
 	common.UUID
 }
