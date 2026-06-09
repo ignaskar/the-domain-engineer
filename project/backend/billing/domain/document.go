281dd6e8c7225deba5b92f88d212d8bcebe9ef64
Three Dots Labs <contact@threedotslabs.com>
2026-06-09 20:54:37 +0300

    override with example solution for 05-domain-modeling/03-project-issue-receipt-domain

--- project/backend/billing/domain/document.go
+++ project/backend/billing/domain/document.go
@@ -2,23 +2,12 @@ package domain
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"eats/backend/common"
 	"eats/backend/common/shared"
 )
-
-type Document struct {
-	documentUUID      DocumentUUID
-	documentType      DocumentType
-	documentNumber    DocumentNumber
-	externalReference *string
-	issueDate         time.Time
-	currency          shared.Currency
-	seller            LegalEntity
-	buyer             LegalEntity
-	lineItems         []LineItem
-}
 
 type DocumentType struct {
 	common.Enum[DocumentTypeValues]
@@ -32,105 +21,53 @@ func (DocumentTypeValues) Values() []string {
 
 var DocumentTypeReceipt = common.MustEnum[DocumentType]("receipt")
 
-func (d Document) UUID() DocumentUUID {
-	return d.documentUUID
-}
-
-func (d Document) DocumentType() DocumentType {
-	return d.documentType
-}
-
-func (d Document) DocumentNumber() DocumentNumber {
-	return d.documentNumber
-}
-
-func (d Document) ExternalReference() *string {
-	return d.externalReference
-}
-
-func (d Document) IssueDate() time.Time {
-	return d.issueDate
-}
-
-func (d Document) Currency() shared.Currency {
-	return d.currency
-}
-
-func (d Document) Seller() LegalEntity {
-	return d.seller
-}
-
-func (d Document) Buyer() LegalEntity {
-	return d.buyer
-}
-
-func (d Document) LineItems() []LineItem {
-	return d.lineItems
-}
-
 func NewReceipt(data NewDocumentData, docNumber DocumentNumber) (*Document, error) {
-	if data.Buyer.IsZero() {
-		return nil, errors.New("buyer can't be empty")
-	}
-
-	if data.Seller.IsZero() {
-		return nil, errors.New("seller can't be empty")
-	}
-
-	if data.Seller.TaxID() == nil {
-		return nil, errors.New("seller must have a tax ID")
-	}
-
 	if data.Buyer.TaxID() != nil {
 		return nil, errors.New("receipts cannot be issued to buyers with a tax ID")
 	}
-
+	if data.Buyer.IsZero() {
+		return nil, errors.New("buyer can't be empty")
+	}
+	if data.Seller.IsZero() {
+		return nil, errors.New("seller can't be empty")
+	}
 	if data.Currency.IsZero() {
 		return nil, errors.New("currency can't be empty")
 	}
-
 	if data.IssueDate.IsZero() {
 		return nil, errors.New("issue date can't be empty")
 	}
-
-	if data.IssueDate.After(time.Now()) {
+	tomorrow := time.Now().Truncate(24*time.Hour).AddDate(0, 0, 1)
+	if !data.IssueDate.Before(tomorrow) {
 		return nil, errors.New("issue date can't be in the future")
 	}
-
+	if data.Seller.TaxID() == nil {
+		return nil, errors.New("seller must have a tax ID to issue billing documents")
+	}
 	if len(data.LineItems) == 0 {
-		return nil, errors.New("there must be at least one line item")
+		return nil, errors.New("document must have at least one line item")
 	}
 
 	lineItems := make([]LineItem, 0, len(data.LineItems))
-	for _, lineItem := range data.LineItems {
-		if lineItem.Name == "" {
-			return nil, errors.New("name can't be empty")
+	for _, lid := range data.LineItems {
+		lineItem, err := newLineItem(lid)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create line item for document: %w", err)
 		}
-
-		if lineItem.Quantity <= 0 {
-			return nil, errors.New("quantity must be positive")
-		}
-
-		lineItems = append(lineItems, LineItem{
-			lineItemUUID: LineItemUUID{common.NewUUIDv7()},
-			name:         lineItem.Name,
-			quantity:     lineItem.Quantity,
-		})
+		lineItems = append(lineItems, lineItem)
 	}
 
-	document := &Document{
-		documentUUID:      DocumentUUID{common.NewUUIDv7()},
-		documentType:      DocumentTypeReceipt,
-		documentNumber:    docNumber,
+	return &Document{
+		uuid:              DocumentUUID{common.NewUUIDv7()},
 		externalReference: data.ExternalReference,
+		documentType:      DocumentTypeReceipt,
 		issueDate:         data.IssueDate,
+		documentNumber:    docNumber,
 		currency:          data.Currency,
 		seller:            data.Seller,
 		buyer:             data.Buyer,
 		lineItems:         lineItems,
-	}
-
-	return document, nil
+	}, nil
 }
 
 type NewDocumentData struct {
@@ -142,24 +79,6 @@ type NewDocumentData struct {
 	LineItems         []NewLineItemData
 }
 
-type LineItem struct {
-	lineItemUUID LineItemUUID
-	name         string
-	quantity     int
-}
-
-func (l LineItem) UUID() LineItemUUID {
-	return l.lineItemUUID
-}
-
-func (l LineItem) Name() string {
-	return l.name
-}
-
-func (l LineItem) Quantity() int {
-	return l.quantity
-}
-
 type NewLineItemData struct {
 	Name     string
 	Quantity int
@@ -169,6 +88,88 @@ type DocumentUUID struct {
 	common.UUID
 }
 
+type Document struct {
+	uuid              DocumentUUID
+	externalReference *string
+	documentType      DocumentType
+	issueDate         time.Time
+	currency          shared.Currency
+	documentNumber    DocumentNumber
+	seller            LegalEntity
+	buyer             LegalEntity
+	lineItems         []LineItem
+}
+
+func (d *Document) UUID() DocumentUUID {
+	return d.uuid
+}
+
+func (d *Document) ExternalReference() *string {
+	return d.externalReference
+}
+
+func (d *Document) DocumentType() DocumentType {
+	return d.documentType
+}
+
+func (d *Document) DocumentNumber() DocumentNumber {
+	return d.documentNumber
+}
+
+func (d *Document) IssueDate() time.Time {
+	return d.issueDate
+}
+
+func (d *Document) Currency() shared.Currency {
+	return d.currency
+}
+
+func (d *Document) Seller() LegalEntity {
+	return d.seller
+}
+
+func (d *Document) Buyer() LegalEntity {
+	return d.buyer
+}
+
+func (d *Document) LineItems() []LineItem {
+	return d.lineItems
+}
+
 type LineItemUUID struct {
 	common.UUID
+}
+
+type LineItem struct {
+	uuid     LineItemUUID
+	name     string
+	quantity int
+}
+
+func newLineItem(data NewLineItemData) (LineItem, error) {
+	if data.Name == "" {
+		return LineItem{}, errors.New("name can't be empty")
+	}
+
+	if data.Quantity < 1 {
+		return LineItem{}, errors.New("quantity must be positive")
+	}
+
+	return LineItem{
+		uuid:     LineItemUUID{common.NewUUIDv7()},
+		name:     data.Name,
+		quantity: data.Quantity,
+	}, nil
+}
+
+func (l LineItem) UUID() LineItemUUID {
+	return l.uuid
+}
+
+func (l LineItem) Name() string {
+	return l.name
+}
+
+func (l LineItem) Quantity() int {
+	return l.quantity
 }
