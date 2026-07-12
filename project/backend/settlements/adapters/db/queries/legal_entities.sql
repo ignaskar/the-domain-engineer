1ccf5d10d37904a7b411a8f35890832bfc99ea4d
Three Dots Labs <contact@threedotslabs.com>
2026-07-12 20:56:12 +0300

    init files for 09-bootstrap-settlements/04-project-create-partner

--- project/backend/settlements/adapters/db/queries/legal_entities.sql
+++ project/backend/settlements/adapters/db/queries/legal_entities.sql
@@ -1,6 +1,29 @@
--- TODO: write two queries.
+-- name: LegalEntityByUUID :one
+SELECT * FROM settlements.legal_entities WHERE legal_entity_uuid = $1;
+
+-- name: SaveLegalEntity :exec
+INSERT INTO settlements.legal_entities (legal_entity_uuid, legal_entity_type, business_name, tax_id, address, bank_account_number, currency)
+VALUES (
+           sqlc.arg(legal_entity_uuid), sqlc.arg(legal_entity_type),
+           sqlc.arg(business_name), sqlc.arg(tax_id),
+           sqlc.arg(address),
+           sqlc.arg(bank_account_number), sqlc.arg(currency)
+       )
+ON CONFLICT (legal_entity_uuid) DO UPDATE SET
+                          business_name = EXCLUDED.business_name,
+                          tax_id = EXCLUDED.tax_id,
+                          address = EXCLUDED.address,
+                          bank_account_number = EXCLUDED.bank_account_number,
+                          currency = EXCLUDED.currency,
+                          updated_at = NOW();
+
+-- TODO: add two queries below.
 --
--- 1. LegalEntityByUUID :one  -- SELECT a single row from settlements.legal_entities by legal_entity_uuid.
--- 2. SaveLegalEntity :exec   -- INSERT into settlements.legal_entities, ON CONFLICT update the mutable fields.
+-- 1. SavePartnerPlatformMapping :exec
+--    INSERT into settlements.partner_platform_mappings.
+--    Use ON CONFLICT (partner_uuid) DO NOTHING (idempotent).
+--
+-- 2. PlatformByPartnerUUID :one
+--    SELECT platform_entity_uuid from settlements.partner_platform_mappings WHERE partner_uuid = $1.
 --
 -- After writing the queries, run `task generate` to regenerate dbmodels.
