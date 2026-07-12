201352b70cd963bb8fd0959ad6956c4340364820
Three Dots Labs <contact@threedotslabs.com>
2026-07-12 17:54:12 +0300

    completed 09-bootstrap-settlements/01-project-iban

--- project/backend/settlements/domain/iban.go
+++ project/backend/settlements/domain/iban.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"errors"
+	"unicode"
+)
+
 // TODO: design the IBAN value object.
 //
 // You'll need to add:
@@ -18,3 +23,48 @@ package domain
 // - UnmarshalIBAN(string) IBAN constructor used by the persistence layer
 //   when loading an already-validated IBAN from the database (no validation,
 //   no error). Look at how other value objects in the project expose this.
+
+type IBAN struct {
+	iban string
+}
+
+func NewIBAN(iban string) (IBAN, error) {
+	if iban == "" {
+		return IBAN{}, errors.New("IBAN empty")
+	}
+
+	if len(iban) < 15 {
+		return IBAN{}, errors.New("IBAN too short")
+	}
+
+	if len(iban) > 34 {
+		return IBAN{}, errors.New("IBAN too long")
+	}
+
+	for i, s := range iban {
+		if i == 0 || i == 1 {
+			if !unicode.IsLetter(s) {
+				return IBAN{}, errors.New("first two IBAN characters must be letters")
+			}
+			continue
+		}
+
+		if !unicode.IsDigit(s) && !unicode.IsLetter(s) {
+			return IBAN{}, errors.New("remaining IBAN characters must be alphanumeric")
+		}
+	}
+
+	return IBAN{iban: iban}, nil
+}
+
+func (i IBAN) IsZero() bool {
+	return i.iban == ""
+}
+
+func (i IBAN) String() string {
+	return i.iban
+}
+
+func UnmarshalIBAN(iban string) IBAN {
+	return IBAN{iban: iban}
+}
